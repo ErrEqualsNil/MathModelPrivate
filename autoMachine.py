e606fc80cc9949d3508102cb87d9eb0ba05d46cb
XinShiQaQ <811681902@qq.com>
2019-12-12 19:06:10 +0800

    Big change of deciding moving

--- autoMachine.py
+++ autoMachine.py
@@ -1,51 +1,65 @@
 import Astar
 import copy
 import queue
-direction = [[1, 0], [-1, 0], [0, 1], [0, -1], [0, 0]]
+direction = [[1, 0], [-1, 0], [0, 1], [0, -1]]
 
 
-def movechoose(m, tmp, x, y, person, number, maxx, maxy):
-    ret = [0, 0, 0, 0, 0]
-    if number == 0:
-        return ret
-    v = 1  # adult
-    if person == 0:
-        v = 2  # old
-    elif person == 1:
-        v = 1.5  # young
-    elif person == 3:
-        v = 3  # disabled
-    moveProb = [0, 0, 0, 0, 0]
-    maxProb_ID = 0
+class weight:
+    def __init__(self, w, index):
+        self.weight = w
+        self.index = index
+    def __cmp__(self, other):
+        if self.weight == other.weight:
+            return self.index < other.index
+        return self.weight > other.weight
+    def __lt__(self, other):
+        if self.weight == other.weight:
+            return self.index < other.index
+        return self.weight > other.weight
 
-    for i in range(5):
-        newx = x + direction[i][0]
-        newy = y + direction[i][1]
-        if newx < 0 or newx >= maxx or newy < 0 or newy >= maxy or m[newx][newy].cap == 0:
+
+def movechoose(m, tmp, x, y, maxx, maxy):
+    for ID in range(4):
+        if m[x][y].person[ID] == 0:
             continue
-        remain_vol = tmp[newx][newy].remain_vol()
-        if remain_vol <= 0:
-            continue
-        right = (m[x][y].msg - m[newx][newy].msg) * v * 2 + remain_vol
-        if right > moveProb[maxProb_ID]:
-            maxProb_ID = i
-        if right < 0:
-            right = 0
-        moveProb[i] = right
-    Probsum = 0
-    for e in moveProb:
-        Probsum += e
-    for i in range(5):
-        try:
-            ret[i] = int((number * moveProb[i]) / Probsum)
-        except ZeroDivisionError:
-            ret[i] = 0
-    for i in range(5):
-        number -= ret[i]
-    if number > 0:
-        ret[maxProb_ID] += number
-    return ret
+        alpha = 0
+        if ID == 0:
+            alpha = 0.7  # old
+        elif ID == 1:
+            alpha = 0.5  # young
+        elif ID == 2:
+            alpha = 0.3  # adult
+        elif ID == 3:
+            alpha = 1  # disabled
 
+        weightlist = queue.PriorityQueue()
+        for i in range(4):
+            newx = x + direction[i][0]
+            newy = y + direction[i][1]
+            if newx < 0 or newx >= maxx or newy < 0 or newy >= maxy or m[newx][newy].cap == 0:
+                w = -1
+                weightlist.put(weight(w, i))
+                continue
+            if m[newx][newy].exit:
+                w = 0x3f3f
+            else:
+                b = tmp[newx][newy].totalPerson() / m[newx][newy].cap
+                theta = m[x][y].msg - m[newx][newy].msg
+                w = theta - (alpha * b - 0.5)
+            weightlist.put(weight(w, i))
+        personNeedMove = m[x][y].person[ID]
+        for i in range(4):
+            w = weightlist.get()
+            if w.weight <= 0:
+                break
+            newx = x + direction[w.index][0]
+            newy = y + direction[w.index][1]
+            change = min(tmp[newx][newy].remain_vol(), personNeedMove)
+            tmp[newx][newy].person[ID] += change
+            tmp[x][y].person[ID] -= change
+            personNeedMove -= change
+            if m[x][y].person[ID] == 0:
+                break
 
 def process(iter_time):
     flows = 3
@@ -87,21 +101,11 @@ def process(iter_time):
                     print("{} leave at x = {};y = {}; remain {}".format(flows - needflow,
                                                                         i, j, tmp[i][j].totalPerson()))
             else:
-                for person in range(4):
-                    change = movechoose(m, tmp, i, j, person, m[i][j].person[person], x, y)
-                    for k in range(5):
-                        newx = i + direction[k][0]
-                        newy = j + direction[k][1]
-                        if newx < 0 or newx >= x or newy < 0 or newy >= y or m[newx][newy].cap == 0:
-                            continue
-                        change[k] = min(tmp[newx][newy].remain_vol(), change[k])
-                        tmp[newx][newy].person[person] += change[k]
-                        tmp[i][j].person[person] -= change[k]
-                        if change[k] != 0:
-                            print("{} move from x = {};y = {} to x = {};y = {}".format(change[k], i, j, newx, newy))
+                movechoose(m, tmp, i, j, x, y)
             for flag in range(4):
                 newnode = Astar.pos(i + direction[flag][0], j + direction[flag][1])
-                if newnode.x < 0 or newnode.y < 0 or newnode.x >= x or newnode.y >= y or vis[newnode.x][newnode.y] != 0:
+                if newnode.x < 0 or newnode.y < 0 or newnode.x >= x or newnode.y >= y or \
+                        vis[newnode.x][newnode.y] != 0 or m[newnode.x][newnode.y].totalPerson() == 0:
                     continue
                 vis[newnode.x][newnode.y] = 1
                 q.put(newnode)
@@ -116,9 +120,7 @@ def process(iter_time):
 
 
 itertimes = input("Please input time of iterations:")
-try:
-    itertimes = int(itertimes)
-    process(itertimes)
-except:
-    print("Invalid Input! Please try again\n")
+itertimes = int(itertimes)
+process(itertimes)
+
 
